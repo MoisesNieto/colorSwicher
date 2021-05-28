8bdd6626e29a7f418176482cd851c0fc749a2408
Moises Nieto <enieto19@hotmail.com>
2021-05-28 20:54:03 +0200

    color-switcher-finally

---
+++ app.js
@@ -0,0 +1,20 @@
+"use strict";
+const hexValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+btn.addEventListener('click', () => {
+    generateColor(colorChange => {
+        color.textContent = colorChange;
+        document.body.style.backgroundColor = colorChange;
+    });
+});
+function generateColor(update) {
+    let hexColor = '#';
+    for (let i = 0; i < 6; i++) {
+        hexColor += hexValues[getRandomColor()];
+    }
+    update(hexColor);
+}
+function getRandomColor() {
+    return Math.floor(Math.random() * hexValues.length);
+}
